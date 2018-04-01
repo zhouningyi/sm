927417741c316cd9f51ad8128a70b212343eadeb
senir <304875853@qq.com>
2018-04-01 23:49:14 +0800

    :art: m

--- shell/coincola.sh
+++ shell/coincola.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for((i=1;i<=20;i++));do
-  /usr/local/bin/node /Users/yajie/Documents/dingxue/sm/index.js  -n coincola_platform  --clean >> /Users/yajie/Documents/dingxue/time.txt;
-  sleep 3
-done
+/usr/local/bin/node /Users/yajie/Documents/dingxue/sm/index.js  -n coincola_platform  --clean >> /Users/yajie/Documents/dingxue/time.txt;
+
+# for((i=1;i<=20;i++));do
+  # /usr/local/bin/node /Users/yajie/Documents/dingxue/sm/index.js  -n coincola_platform  --clean >> /Users/yajie/Documents/dingxue/time.txt;
+  # sleep 3
+# done
